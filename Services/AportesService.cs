dc2c724fea5ef0c44d418368fe3e0dcd50114f85
Imanol-Acosta <ilacosta05@hotmail.com>
2025-06-09 19:36:03 -0400

    Agregando existe, insertar y modificar

--- Services/AportesService.cs
+++ Services/AportesService.cs
@@ -1,10 +1,30 @@
 ﻿using Imanol_Acosta_Ap1_P1.DAL;
+using Imanol_Acosta_Ap1_P1.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Imanol_Acosta_Ap1_P1.Services;
 
-    public class AportesService
+public class AportesService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Existe(int AporteID)
     {
-    public AportesService(IDbContextFactory<Contexto> dbFactory) { }
+        await using var Contexto = await DbFactory.CreateDbContextAsync();
+        return await Contexto.Aportes.AsNoTracking().AnyAsync(a => a.AporteID == AporteID);
     }
+
+    private async Task<bool> Insertar(Aporte aportes)
+    {
+        await using var Contexto = await DbFactory.CreateDbContextAsync();
+        Contexto.Aportes.Add(aportes);
+        return await Contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Modificar(Aporte aportes)
+    {
+        await using var Contexto = await DbFactory.CreateDbContextAsync();
+        Contexto.Aportes.Update(aportes);
+        return await Contexto.SaveChangesAsync() > 0;
+    }
+
+
+}
 
