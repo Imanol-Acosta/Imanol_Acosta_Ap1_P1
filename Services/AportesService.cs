7cc76a0699a3d342d8ecf427c51e9e93718299ef
Imanol-Acosta <ilacosta05@hotmail.com>
2025-06-09 18:58:19 -0400

    Agregando AportesService

---
+++ Services/AportesService.cs
@@ -0,0 +1,10 @@
+﻿using Imanol_Acosta_Ap1_P1.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imanol_Acosta_Ap1_P1.Services;
+
+    public class AportesService
+    {
+    public AportesService(IDbContextFactory<Contexto> dbFactory) { }
+    }
+
